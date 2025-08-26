bf505998340407607c8f138ecfc709ab3a90e4ce
Rayan <rayanalhassan2000@gmail.com>
2025-08-26 15:49:43 +0300

    clear

--- pages/career/index.tsx
+++ pages/career/index.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
