4eb84a322f96c43bc28e9fafb1a7193122ef5c52
Rayan <rayanalhassan2000@gmail.com>
2025-09-01 17:41:17 +0300

    clean

--- pages/career/index.tsx
+++ pages/career/index.tsx
@@ -3,8 +3,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import Lottie from "lottie-react";
